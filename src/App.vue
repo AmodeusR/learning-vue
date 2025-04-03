ab608d3ec5f544f706f893daad85876673bc98f9
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-03 19:47:22 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Tasks from "@/components/Tasks.vue";
+  import CreateTask from "./components/CreateTask.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="wrapper">
+    <h1>Tasks</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <CreateTask />
+      <Tasks />
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .wrapper {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
+    align-items: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  main {
+    width: min(40rem, 100%);
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  h1 {
+    font-size: 3rem;
   }
-}
 </style>
