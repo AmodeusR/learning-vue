e8d84ca9df32da5d74b93e4a6a221f76a7a7a200
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-03 19:47:12 -0300

    Create Tasks.vue

---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import TaskItem from "./TaskItem.vue";
+</script>
+
+<template>
+  <ul>
+    <TaskItem title="Walk the dogs" description="Take the dogs for a walk" />
+    <TaskItem title="Walk the dogs" description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et quae consectetur eum facere dolorem molestias velit sequi nobis culpa autem.
+" />
+  </ul>
+</template>
+
+<style>
+  ul {
+    width: 100%;
+    padding: 0;
+  }
+</style>
