1fc6f426b8427c644796482c497e0e264f17be09
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-03 19:47:08 -0300

    Create TaskItem.vue

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+  </li>
+</template>
+
+<script setup lang="ts">
+  const { title, description } = defineProps<{
+    title: string;
+    description?: string;
+  }>();
+
+</script>
+
+<style scoped>
+  h2 {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+
+  p {
+    text-indent: 20px;
+  }
+
+  li {
+    width: 100%;
+    list-style: none;
+    margin: 1rem 0;
+    padding: .5rem 1rem;
+    border: 1px solid #ccc4;
+    border-radius: 4px;
+    transition: 200ms border-color;
+  }
+
+  li:hover {
+    border-color: #ccc8
+  }
+</style>
