28ce019773a9e7fd78083ceb3a5e2f112319f2bb
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-13 21:39:36 -0300

    integrate localstorage for tasks

--- simple-tasks/src/App.vue
+++ simple-tasks/src/App.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-  import Tasks from "@/components/Tasks.vue";
-  import CreateTask from "./components/CreateTask.vue";
-  import type { Task } from "./components/types";
-  import { ref } from "vue";
+import Tasks from "@/components/Tasks.vue";
+import CreateTask from "./components/CreateTask.vue";
+import type { Task } from "./components/types";
+import { useLocalStorage } from "@vueuse/core";
 
-  const tasks = ref<Task[]>([]);
 
-  const createTask = (task: Task) => {
-    tasks.value.push(task)
-  }
 
-  const deleteTask = (id: string) => {
-    tasks.value = tasks.value.filter(task => task.id !== id);
-  }
+const tasks = useLocalStorage<Task[]>("tasks", []);
+
+const createTask = (task: Task) => {
+  tasks.value.push(task);
+};
+
+const deleteTask = (id: string) => {
+  tasks.value = tasks.value.filter((task) => task.id !== id);
+};
 </script>
 
 <template>
@@ -27,16 +29,16 @@
 </template>
 
 <style scoped>
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  main {
-    width: min(40rem, 100%);
-  }
-  h1 {
-    font-size: 3rem;
-  }
+main {
+  width: min(40rem, 100%);
+}
+h1 {
+  font-size: 3rem;
+}
 </style>
